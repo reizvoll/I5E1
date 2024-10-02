c92541749da5405a5ce5f951a924a06f47db9c65
WonYunSun <ereunosun113@gmail.com>
2024-10-02 20:02:56 +0900

    feat : 방명록 작성시간 처리

--- js/comment.js
+++ js/comment.js
@@ -14,7 +14,8 @@ function submitForm(e) {
 // 방명록 저장 함수
 const saveComment = (content) => {
     var newComment = commentDb.push();
-    const timestamp = new Date(Date.now()).toLocaleString(); //작성시간, 시간 포매팅 해놨어요 필요없음 지우세요
+    // const timestamp = new Date(Date.now()).toLocaleString(); //작성시간, 시간 포매팅 해놨어요 필요없음 지우세요
+    const timestamp = Date.now(); //포메팅 지움.
     newComment
         .set({ comment: content, createAt: timestamp }) // 방명록 저장
         .then(() => {
@@ -26,6 +27,7 @@ const saveComment = (content) => {
         });
 };
 
+
 // DOM 요소 값 얻어오는 함수
 const getElementVal = (id) => {
     return document.getElementById(id).value; // 해당 ID의 값 반환
@@ -34,6 +36,15 @@ const getElementVal = (id) => {
 document.addEventListener('DOMContentLoaded', function () {
     commentDb.on('child_added', (data) => {
         const commentData = data.val();
+
+        //입력 날짜 비교
+        let createAtDate = new Date(commentData.createAt);
+        let currentDate = new Date();
+        
+        // 시간 차이 계산 (밀리초 단위 -> 분 단위로 변환)
+        let timeDiff= Math.floor((currentDate - createAtDate) / (1000 * 60)); // 밀리초를 분 단위로 변환
+        let timeago = formatTimeAgo(timeDiff, createAtDate);
+
 
         // 이전 내역 임시 저장
         let prev_comment = document.getElementById('guestbook').innerHTML;
@@ -45,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div>
                     <div class="guestbook-user">
                         <p class="guestbook-name">방문자</p>
-                        <p class="guestbook-date">${commentData.createAt}</p>
+                        <p class="guestbook-date">${timeago}</p>
                     </div>
                     <p class="guestbook-comment">${commentData.comment}</p>
                 </div>
@@ -56,3 +67,16 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('guestbook').innerHTML += prev_comment;
     });
 });
+
+
+function formatTimeAgo(timeDiff, originalDate) {
+        if (timeDiff < 60) {
+            if (timeDiff < 1) return "방금 전";
+            return `${timeDiff}분 전`;
+        } else if (timeDiff < 60 * 5) { // Less than 5 hours
+            const hoursDiff = Math.floor(minutesDiff / 60);
+            return `${hoursDiff}시간 전`;
+        } else {
+            return new Date(originalDate).toLocaleString(); // Show the original date if more than 5 hours
+        }
+    }
