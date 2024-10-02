196ee51a7e03340cdf04b4a41df1e60a1b5f8f0a
WonYunSun <ereunosun113@gmail.com>
2024-10-02 17:25:46 +0900

    feat : 방명록 create 완료

---
+++ js/comment.js
@@ -0,0 +1,33 @@
+// Realtime DB 초기화
+var commentDb = firebase.database().ref('comment');
+
+// 작성 버튼 eventListener
+document.getElementById('commentForm').addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+    var content = getElementVal('commentCont'); 
+    console.log(content);
+    saveComment(content); // 댓글 저장 함수 호출
+}
+
+// 방명록 저장 함수
+const saveComment = (content) => {
+    var newComment = commentDb.push();
+    const timestamp = new Date(Date.now()).toLocaleString(); //작성시간, 시간 포매팅 해놨어요 필요없음 지우세요
+    newComment.set({ comment: content,
+        createAt:timestamp,
+    }) // 방명록 저장
+        .then(() => {
+            console.log("방명록 저장 성공~:", content , timestamp);
+            document.getElementById("commentCont").value = ''; // 입력 필드 비우기
+        })
+        .catch((error) => {
+            console.error("방명록 저장 실패:", error);
+        });
+}
+
+// DOM 요소 값 얻어오는 함수
+const getElementVal = (id) => {
+    return document.getElementById(id).value; // 해당 ID의 값 반환
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB761P86qZDtQXgvjXNes4GRzhzVq42tLI",
+    authDomain: "i5e1-miniproject.firebaseapp.com",
+    projectId: "i5e1-miniproject",
+    databaseURL: "https://i5e1-miniproject-default-rtdb.firebaseio.com/",
+    storageBucket: "i5e1-miniproject.appspot.com",
+    messagingSenderId: "870517772024",
+    appId: "1:870517772024:web:b6920a4f26ec058865b50d",
+    measurementId: "G-XJV228R1Z5"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
